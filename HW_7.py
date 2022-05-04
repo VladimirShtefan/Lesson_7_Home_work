17ccd9b5d7e358aee363e386d0e1a20d0d5c96a1
Vladimir Shtefan <shtefanvladimir14@gmail.com>
2022-05-04 21:17:59 +0700

    Чистый проект

--- HW_7.py
+++ HW_7.py
@@ -10,10 +10,13 @@ def main():
     # вызываем функции которые возвращают данные из файлов в виде списка со словарями
     dict_all_students = functions.load_json(Path_file_students)
     dict_all_professions = functions.load_json(Path_file_professions)
+
     # спрашиваем у пользователя номер студента
     user_input = input('Добро пожаловать.\nВведите номер студента: ')
+
     # вызываем функцию, которая возвращает данные о студенте в виде словаря
     dict_student = functions.get_student_by_pk(user_input, dict_all_students)
+
     # проверяем что вернулась не пустота
     if dict_student is not None:
         # выводим информацию
@@ -21,10 +24,13 @@ def main():
         print(f'Студент {full_name}')
         skills = ", ".join(dict_student["skills"])
         print(f'Знает: {skills}')
+
         # спрашиваем о профессии
         user_profession = input(f'Выберите специальность для оценки студента {full_name}: ')
+
         # вызываем функцию, которая возвращает данные о выбранной профессии
         dict_profession = functions.get_profession_by_title(user_profession, dict_all_professions)
+
         # проверяем что вернулась не пустота
         if dict_profession is not None:
             # вызываем функцию для обработки пригодности и выводим результаты
